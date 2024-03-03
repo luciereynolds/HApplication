8d466098926093e6277cfa36e83b79829727128a
lucie <lreyno202@caledonian.ac.uk>
2024-03-03 17:02:21 +0000

    fixed overhang link from branding

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -5,14 +5,10 @@ const Navigation = () => {
   return (
     <div className="container-fluid">
       <div className="container">
-        <nav className="navbar navbar-expand-lg navbar-light py-2 py-lg-0 d-flex align-items-center">
-          <a href="/" className="navbar-brand" style={{ marginTop: "15px"}}>
-            <img
-              src={Logo}
-              alt="Logo"
-              style={{ height: "50px", marginRight: "10px" }}
-            />
-            <span style={{ fontSize: "20px", fontWeight: "bold" }}>ComputingCadets</span>
+        <nav className="navbar navbar-expand-lg align-items-center">
+          <a href="/" className="navbar-brand" style={{ marginTop: "0.5rem", maxHeight: "45px"}}>
+              <img src={Logo} alt="Logo" style={{ height: "50px", marginRight: "10px" }}/>
+              <span style={{ fontSize: "20px", fontWeight: "bold" }}>ComputingCadets</span>
           </a>
 
           <button
@@ -26,13 +22,13 @@ const Navigation = () => {
 
           <div className="collapse navbar-collapse" id="navbarCollapse">
             <div className="navbar-nav mx-auto">
-              <a href="/" className="nav-item nav-link active">
+              <a href="/" className="nav-item nav-link active" style={{ padding:"0.5rem"}}>
                 Home
               </a>
-              <a href="/lessons" className="nav-item nav-link">
+              <a href="/lessons" className="nav-item nav-link" style={{ padding:"0.5rem"}}>
                 Lessons
               </a>
-              <a href="/leaderboard" className="nav-item nav-link">
+              <a href="/leaderboard" className="nav-item nav-link" style={{ padding:"0.5rem"}}>
                 Leaderboard
               </a>
             </div>
