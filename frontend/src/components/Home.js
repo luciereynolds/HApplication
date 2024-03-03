f54ab5026d1768e57521536c3fc4dd24b2589dc0
lucie <lreyno202@caledonian.ac.uk>
2024-03-03 15:27:57 +0000

    update

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,11 +5,11 @@ const Home = () => {
     <div className="container-fluid">
       <div className="row">
       <div className="col p-3 mx-2">
-        <h3>Welcome to the exciting world of National 4 Computer Science! 🚀</h3>
+        <h3>Welcome to the exciting world of National 4 Computer Science!</h3>
         <br></br>
         <p>We understand that National 4 Computer Science can seem like a labyrinth of new concepts and terminology. But fear not! Our mission is to transform learning into an adventure, where you'll discover the magic behind computers, explore the language of coding, and unleash your creativity in the digital landscape.</p>
-        <p>Feel free to explore our lessons and then, see where you rank on our leaderboard! How many trophies can you earn?🏆</p>
-        <p>So, are you ready to level up your computer science skills? Let the adventure begin! 🚀💻</p>
+        <p>Feel free to explore our lessons and then, see where you rank on our leaderboard! How many trophies can you earn?</p>
+        <p>So, are you ready to level up your computer science skills? Let the adventure begin!</p>
       </div>
       </div>
     </div>
