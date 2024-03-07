afc9b2a6ef0c38025ef6da8b01ca8ece37c50e3c
lucie <lreyno202@caledonian.ac.uk>
2024-03-07 09:21:59 +0000

    Layout change of lessons page

--- frontend/src/components/MainLessons.js
+++ frontend/src/components/MainLessons.js
@@ -3,22 +3,31 @@ import React from "react";
 const Lessons = () => {
 
   return (
-    <div className="container-fluid">
-      <h3>Explore our lessons!🚀</h3>
+    <div className="container-fluid" style={{ padding: "50px", paddingTop: "0px" }}>
       <div className="row">
-        <div className="col p-3 mx-2">
-          <h4><a href="/lessons/datatypesandstructures" className="nav-item nav-link">
-            Data Types and Structures
-          </a></h4>
-          <p>Embark on a coding journey! Discover programming basics with numbers, words, and structures.</p>
-        </div>
-        <div className="col p-3 mx-2">
-          <h4>Low Level Operations and Computer Architecture </h4>
-          <p>Discover computing essentials: binary, memory, CPU. Decode low-level operations, explore computer architecture. Unveil tech secrets in a nutshell!</p>
-        </div>
-        <div className="col p-3 mx-2">
-          <h4>Testing and Documenting Solutions </h4>
-          <p>Level up coding skills! Learn to test and document like a pro. Make sure your solutions rock and your code tells the story!</p>
+        <div className="col-md p-3 mx-2 text-dark" style={{ padding: "0.5rem" }}>
+          <h3>Explore our lessons!🚀</h3>
+          <br></br>
+          <div className="row">
+            <div className="col-md">
+              <a href="/lessons/datatypesandstructures" className="nav-item nav-link">
+                <h4>Data Types and Structures</h4>
+                <p>Embark on a coding journey! Discover programming basics with numbers, words, and structures.</p>
+              </a>
+            </div>
+            <div className="col-md">
+              <a href="/lessons/datatypesandstructures" className="nav-item nav-link">
+                <h4>Low Level Operations and Computing Architecture</h4>
+                <p>Discover computing essentials: binary, memory, CPU. Decode low-level operations, explore computer architecture. Unveil tech secrets in a nutshell!</p>
+              </a>
+            </div>
+            <div className="col-md mx-2">
+              <a href="/lessons/datatypesandstructures" className="nav-item nav-link">
+                <h4>Testing and Documenting Solutions</h4>
+                <p>Level up coding skills! Learn to test and document like a pro. Make sure your solutions rock and your code tells the story!</p>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -26,3 +35,31 @@ const Lessons = () => {
 };
 
 export default Lessons;
+
+
+// delete eventually - just for reference rn
+
+//return (
+//    <div className="container-fluid">
+//      <h3  style={{padding: "0.5rem"}}>Explore our lessons!🚀</h3>
+//      <div className="row" style={{padding: "0.5rem"}}>
+//        <div className="col p-3 mx-2">
+//          <h4>
+//            <a href="/lessons/datatypesandstructures" className="nav-item nav-link">
+//              Data Types and Structures
+//            </a>
+//          </h4>
+//          <p>Embark on a coding journey! Discover programming basics with numbers, words, and structures.</p>
+//        </div>
+//        <div className="col p-3 mx-2">
+//          <h4>Low Level Operations and Computer Architecture </h4>
+//          <p>Discover computing essentials: binary, memory, CPU. Decode low-level operations, explore computer architecture. Unveil tech secrets in a nutshell!</p>
+//        </div>
+//        <div className="col p-3 mx-2">
+//          <h4>Testing and Documenting Solutions </h4>
+//          <p>Level up coding skills! Learn to test and document like a pro. Make sure your solutions rock and your code tells the story!</p>
+//        </div>
+//      </div>
+//    </div>
+//  );
+//};
