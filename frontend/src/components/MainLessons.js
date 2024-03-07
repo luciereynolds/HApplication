8e84288832e57ce3c015e32cce8368c7f29a697f
lucie <lreyno202@caledonian.ac.uk>
2024-03-07 15:16:26 +0000

    styling lessons page

--- frontend/src/components/MainLessons.js
+++ frontend/src/components/MainLessons.js
@@ -7,21 +7,22 @@ const Lessons = () => {
       <div className="row">
         <div className="col-md p-3 mx-2 text-dark" style={{ padding: "0.5rem" }}>
           <h3 style={{textAlign: "center"}}>Explore our lessons!</h3>
+          <p style={{textAlign:"center"}}>Here you can find links to all our lessons. Happy exploring!</p>
           <br></br>
           <div className="row">
-            <div className="col-md mx-2 mb-2" style={{backgroundColor: "#8c7bff", borderRadius: "20px", paddingTop: "20px"}}>
+            <div className="col-md mx-2 mb-2 text-light" style={{backgroundColor: "#6a5bcf", borderRadius: "20px", paddingTop: "20px"}}>
               <a href="/lessons/DataTypesAndStructures" className="nav-item nav-link">
                 <h4 style={{textAlign: "center"}}>Data Types and Structures</h4>
                 <p>Embark on a coding journey! Discover programming basics with numbers, words, and structures.</p>
               </a>
             </div>
-            <div className="col-md mx-2 mb-2" style={{backgroundColor: "#4ada85", borderRadius: "20px", paddingTop: "20px"}}>
+            <div className="col-md mx-2 mb-2 text-light" style={{backgroundColor: "#2eba68", borderRadius: "20px", paddingTop: "20px"}}>
               <a href="/lessons/LowLevelOperation" className="nav-item nav-link">
                 <h4 style={{textAlign: "center"}}>Low Level Operations and Computing Architecture</h4>
                 <p>Discover computing essentials: binary, memory, CPU. Decode low-level operations, explore computer architecture. Unveil tech secrets in a nutshell!</p>
               </a>
             </div>
-            <div className="col-md mx-2 mb-2" style={{backgroundColor: "#fda4fe", borderRadius: "20px", paddingTop: "20px"}}>
+            <div className="col-md mx-2 mb-2 text-light" style={{backgroundColor: "#e978eb", borderRadius: "20px", paddingTop: "20px"}}>
               <a href="/lessons/TestingAndDocumenting" className="nav-item nav-link">
                 <h4 style={{textAlign: "center"}}>Testing and Documenting Solutions</h4>
                 <p>Level up coding skills! Learn to test and document like a pro. Make sure your solutions rock and your code tells the story!</p>
