55c0b3022d08f1c5e8bd4aab2764aa6b9f41cd33
lucie <lreyno202@caledonian.ac.uk>
2024-03-21 15:09:55 +0000

    added extra flip but decimal is upside down

--- frontend/src/components/LowLevelContent/BinaryCounter.js
+++ frontend/src/components/LowLevelContent/BinaryCounter.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 class FlipCounter extends React.Component {
   constructor(props) {
@@ -43,18 +44,33 @@ class FlipCounter extends React.Component {
   render() {
     const { binaryValues, decimalNumber } = this.state;
 
+    // Convert the decimal number to an array of digits
+    const decimalDigits = decimalNumber.toString().split('').map(Number);
+
     return (
       <div className="container-fluid">
         <div className="row">
           <div className="col p-3 mx-2">
             <h3>Binary Counter!</h3>
             <p>Watch as the binary digits change to see how integers are represented!</p>
-            <div>
-              {binaryValues.map((value, index) => (
-                <span key={index} style={{ backgroundColor: "#d3d3d3", padding: "5px", margin: "2px" }}>{value}</span>
-              ))}
-              <span>=</span>
-              <span>{decimalNumber}</span>
+            <div className="row">
+              <div className="col p-3 mx-2" style={{ fontSize: "16pt" }}>
+                <div style={{ display: 'flex' }}>
+                  {/* Render binary digits */}
+                  {binaryValues.map((value, index) => (
+                    <motion.span key={index} className="numberFlip" animate={{ rotateX: value === 0 ? 180 : 0 }}>
+                      {value === 0 ? '0' : '1'}
+                    </motion.span>
+                  ))}
+                  <span className="numberFlip">=</span>
+                  {/* Render decimal digits */}
+                  {decimalDigits.map((digit, index) => (
+                    <motion.span key={`decimal-${index}`} className="numberFlip" animate={{ rotateX: digit % 2 === 0 ? 180 : 0 }}>
+                      {digit}
+                    </motion.span>
+                  ))}
+                </div>
+              </div>
             </div>
           </div>
         </div>
