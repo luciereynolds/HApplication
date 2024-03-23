941e11a8f5d1e4d3a8fe464fe86a35715bfc2c21
lucie <lreyno202@caledonian.ac.uk>
2024-03-23 14:23:43 +0000

    styling connections

--- frontend/src/components/TestingDocContent/WordConnectionTest.js
+++ frontend/src/components/TestingDocContent/WordConnectionTest.js
@@ -2,10 +2,9 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [, setDraggedItem] = useState(null);
+  const [correctMatches, setCorrectMatches] = useState([]);
 
   const handleDragStart = (event, item) => {
-    setDraggedItem(item);
     // Set the data being dragged
     event.dataTransfer.setData("text/plain", item);
   };
@@ -14,40 +13,41 @@ function App() {
     event.preventDefault();
   };
 
-  const handleDrop = (event, targetWord) => {
+  const handleDrop = (event, targetWord, targetDefinition) => {
     event.preventDefault();
     // Retrieve the dragged data
     const droppedItem = event.dataTransfer.getData("text/plain");
     // Check if the dropped item matches the corresponding target word
     if (droppedItem === targetWord) {
       // Handle correct drop
-      alert('Correct!');
-    } else {
-      // Handle incorrect drop
-      alert('Incorrect!');
+      setCorrectMatches([...correctMatches, { word: targetWord, definition: targetDefinition }]);
     }
-    setDraggedItem(null);
+  };
+
+  const isMatched = (word, definition) => {
+    return correctMatches.some(pair => pair.word === word && pair.definition === definition);
   };
 
   return (
     <div className="App">
       <div className="container mt-5">
         <h1>Match the definition to the correct Word</h1>
+        <p>Click and drag the words to the correct definition, they'll go green when they're correct!</p>
         <div className="row">
           <div className="col mx-2">
             <h2>Words</h2>
             <div className="words">
-              <div className="word" draggable="true" onDragStart={(e) => handleDragStart(e, 'Definition 1')}>Normal</div>
-              <div className="word" draggable="true" onDragStart={(e) => handleDragStart(e, 'Definition 2')}>Extreme</div>
-              <div className="word" draggable="true" onDragStart={(e) => handleDragStart(e, 'Definition 3')}>Exceptional</div>
+              <div className={"word" + (isMatched('Definition 1', 'Data that you would expect to work or be accepted and that lies within the range.') ? ' correct-match' : '')} draggable="true" onDragStart={(e) => handleDragStart(e, 'Definition 1')}>Normal</div>
+              <div className={"word" + (isMatched('Definition 2', 'Data at the lower and upper limits of the range.') ? ' correct-match' : '')} draggable="true" onDragStart={(e) => handleDragStart(e, 'Definition 2')}>Extreme</div>
+              <div className={"word" + (isMatched('Definition 3', 'Data that should not be accepted by the program.') ? ' correct-match' : '')} draggable="true" onDragStart={(e) => handleDragStart(e, 'Definition 3')}>Exceptional</div>
             </div>
           </div>
           <div className="col mx-2">
             <h2>Definitions</h2>
             <div className="definitions">
-              <div className="definition" onDragOver={handleDragOver} onDrop={(e) => handleDrop(e, 'Definition 2')}>Data at the lower and upper limits of the range.</div>
-              <div className="definition" onDragOver={handleDragOver} onDrop={(e) => handleDrop(e, 'Definition 3')}>Data that should not be accepted by the program.</div>
-              <div className="definition" onDragOver={handleDragOver} onDrop={(e) => handleDrop(e, 'Definition 1')}>Data that you would expect to work or be accepted and that lies within the range.</div>
+            <div className={"definition" + (isMatched('Definition 3', 'Data that should not be accepted by the program.') ? ' correct-match' : '')} onDragOver={handleDragOver} onDrop={(e) => handleDrop(e, 'Definition 3', 'Data that should not be accepted by the program.')}>Data that should not be accepted by the program.</div>
+              <div className={"definition" + (isMatched('Definition 1', 'Data that you would expect to work or be accepted and that lies within the range.') ? ' correct-match' : '')} onDragOver={handleDragOver} onDrop={(e) => handleDrop(e, 'Definition 1', 'Data that you would expect to work or be accepted and that lies within the range.')}>Data that you would expect to work or be accepted and that lies within the range.</div>
+              <div className={"definition" + (isMatched('Definition 2', 'Data at the lower and upper limits of the range.') ? ' correct-match' : '')} onDragOver={handleDragOver} onDrop={(e) => handleDrop(e, 'Definition 2', 'Data at the lower and upper limits of the range.')}>Data at the lower and upper limits of the range.</div>
             </div>
           </div>
         </div>
