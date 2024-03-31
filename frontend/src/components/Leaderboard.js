3bfdf4cce10f409119f6772c4fb873ea635e98ba
lucie <lreyno202@caledonian.ac.uk>
2024-03-31 00:57:52 +0000

    medals showing in two columns

--- frontend/src/components/Leaderboard.js
+++ frontend/src/components/Leaderboard.js
@@ -6,6 +6,15 @@ import TDSMedal from "../images/TDSMedal.png";
 import TopDog from "../images/TopDogMedal.png";
 
 const Leaderboard = () => {
+  // Array of medal data
+  const medals = [
+    { image: AllLessonsMedal, alt: "Gold medal for completing all lessons", text: "Completed all lessons!" },
+    { image: DTSMedal, alt: "Medal for completing Data Type Lesson", text: "Completed Data Types Lesson!" },
+    { image: LLOMedal, alt: "Medal for completing Low Level Lesson", text: "Completed Low Level Lesson!" },
+    { image: TDSMedal, alt: "Medal for completing Testing Lesson", text: "Completed Testing Lesson!" },
+    { image: TopDog, alt: "Medal for being at the top of the leaderboard", text: "Top Dog! You're top of the Leaderboard!" }
+  ];
+
   return (
     <div className="container-fluid" style={{ padding: "50px", paddingTop: "0px" }}>
       <div className="row">
@@ -16,38 +25,20 @@ const Leaderboard = () => {
             <div className="col-md">
               <h4>How well do you rank?</h4>
             </div>
-            <div className="col-md">
+            <div className="col-md p-3 mx-2 text-dark" style={{ padding: "0.5rem", textAlign: "center" }}>
               <h4>Your Achievements!🏅</h4>
-              <img
-            src={AllLessonsMedal}
-            alt="Gold medal for completing all lessons"
-            style={{ maxWidth: "20%", height: "auto" }}
-          />
-          <p>Completed all lessons!</p>
-          <img
-            src={DTSMedal}
-            alt="Medal for completing Data Type Lesson"
-            style={{ maxWidth: "20%", height: "auto" }}
-          />
-          <p>Completed Data Types Lesson!</p>
-          <img
-            src={LLOMedal}
-            alt="Medal for completing Low Level Lesson"
-            style={{ maxWidth: "20%", height: "auto" }}
-          />
-          <p>Completed Low Level Lesson!</p>
-          <img
-            src={TDSMedal}
-            alt="Medal for completing Testing Lesson"
-            style={{ maxWidth: "20%", height: "auto" }}
-          />
-          <p>Completed Testing Lesson!</p>
-          <img
-            src={TopDog}
-            alt="Medal for being at the top of the leaderboard"
-            style={{ maxWidth: "20%", height: "auto" }}
-          />
-          <p>Top Dog! You're top of the Leaderboard!</p>
+              <div style={{ display: "grid", gridTemplateColumns: "repeat(2, 1fr)", gap: "20px" }}>
+                {medals.map((medal, index) => (
+                  <div key={index}>
+                    <img
+                      src={medal.image}
+                      alt={medal.alt}
+                      style={{ width: "50%", height: "auto" }}
+                    />
+                    <p>{medal.text}</p>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
@@ -57,19 +48,3 @@ const Leaderboard = () => {
 };
 
 export default Leaderboard;
-
-// delete later - just for reference
-
-//return (
-//  <div className="container-fluid">
-//    <h3>Leaderboard 🏆</h3>
-//    <div className="row">
-//    <div className="col p-3 mx-2">
-//      <h4>How well do you rank? </h4>
-//    </div>
-//    <div className="col p-3 mx-2">
-//      <h4>Your Achievements!🏅</h4>
-//    </div>
-//    </div>
-//  </div>
-//);
